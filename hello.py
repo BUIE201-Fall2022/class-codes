58a423c941122ab363047ca86dd9656e8227ae65
zctaskin <zctaskin@gmail.com>
2022-10-04 13:46:50 +0300

    primitive types

--- hello.py
+++ hello.py
@@ -1,3 +1,34 @@
 print ("Hello, IE 201!")
 name = input ("What is your name?")
 print ("Hello, " + name)
+
+# This is a comment
+
+"""
+    This is a multi-line comment
+    That can span multiple lines
+"""
+
+# Variable types
+# String
+my_string = "This is a string"
+my_string2 = 'This is also a string'
+my_string3 = "It's beautiful out there!"
+
+# int
+my_int = 3
+age_string = input ("How old are you?")
+print(type(age_string))
+age = int(age_string)
+print(type(age))
+
+# float
+my_float = 3.45
+print(type(my_float))
+
+# boolean
+my_bool = True
+my_bool2 = False
+print(type(my_bool))
+
+# data collections
