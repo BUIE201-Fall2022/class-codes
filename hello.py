d7024c8a2ffa1b879f2ee403661eb60371c95c72
zctaskin <zctaskin@gmail.com>
2022-10-11 13:41:52 +0300

    default values

--- hello.py
+++ hello.py
@@ -4,11 +4,25 @@ def function_name():
 
     # variables defined within functions are local unless explicitly declared global
     # do not use global variables!
-    global i
+    # global i
     i = 10
     print("i: " + str(i))
 
 i = 5
 function_name()
 print("i: " + str(i))
+
+def f(a):
+    print(a)
+
+f("Hello, f")
+f(10)
+
+# optional parameter with a default value
+def g(a, b = 5):
+    print("a:", a)
+    print("b:", b)
+
+g(10, 10)
+g(20)
 
