e123f25549e90caa8cad50f33e5daf3ec289f93f
zctaskin <zctaskin@gmail.com>
2022-10-11 14:28:30 +0300

    recursive function

--- hello.py
+++ hello.py
@@ -1,57 +1,22 @@
-def function_name():
-    # function body
-    print("Hello from function_name")
+def factorial(n):
+    result = 1
+    current = 1
+    while current <= n:
+        result *= current
+        current += 1
+    return result
 
-    # variables defined within functions are local unless explicitly declared global
-    # do not use global variables!
-    # global i
-    i = 10
-    print("i: " + str(i))
+five_factorial = factorial(5)
+print("5! = " + str(five_factorial))
 
-i = 5
-function_name()
-print("i: " + str(i))
+def factorial_recursive(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial_recursive(n-1)
 
-def f(a):
-    print(a)
+five_factorial_recursive = factorial_recursive(5)
+print("5! (recursive) = " + str(five_factorial_recursive))
 
-f("Hello, f")
-f(10)
 
-# optional parameter with a default value
-# default arguments have to come at the end
-def g(a = 25, b = 15):
-    print("a:", a)
-    print("b:", b)
-
-# positional argument passing
-g(10, 10)
-g(20)
-g()
-
-# parameter names explicitly given
-g(a = 8, b = 4)
-g(a = 9)
-g(b = 45)
-
-def sum_all(values):
-    total = 0
-    for i in values:
-        total += i
-    return total
-
-total = sum_all([4, 5, 6])
-print(total)
-
-def sum_all2(a, *values):
-    print("a: ", a)
-    total = 0
-    for i in values:
-        total += i
-    return total
-
-total2 = sum_all2("x", 4, 5, 6)
-print(total2)
-
-total2 = sum_all2("y", 4, 5)
-print(total2)
+    
