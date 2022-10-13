1db997a6d689c94f38ad99c05e567c24ef63098e
zctaskin <zctaskin@gmail.com>
2022-10-13 10:09:19 +0300

    iterative fibonacci

--- hello.py
+++ hello.py
@@ -1,29 +1,39 @@
 import time
 
-def factorial(n):
-    result = 1
-    current = 1
-    while current <= n:
-        result *= current
-        current += 1
-    return result
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    prevprev = 1
+    prev = 1
+    current = 2
+    i = 3 # iteration counter
+    while i <= n:
+        current = prev + prevprev
+        prevprev = prev
+        prev = current
+        i += 1
+    return current
+
+fib5 = fibonacci(5)
+
+
 
 N = 990
-iterative_start = time.time()
-five_factorial = factorial(N)
-iterative_end = time.time()
-print("N! = " + str(five_factorial) + " computation time: " + str(iterative_end - iterative_start))
+# iterative_start = time.time()
+# five_factorial = factorial(N)
+# iterative_end = time.time()
+# print("N! = " + str(five_factorial) + " computation time: " + str(iterative_end - iterative_start))
 
-def factorial_recursive(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial_recursive(n-1)
+# def factorial_recursive(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * factorial_recursive(n-1)
 
-recursive_start = time.time()
-five_factorial_recursive = factorial_recursive(N)
-recursive_end = time.time()
-print("N! = " + str(five_factorial_recursive) + " computation time: " + str(recursive_end - recursive_start))
+# recursive_start = time.time()
+# five_factorial_recursive = factorial_recursive(N)
+# recursive_end = time.time()
+# print("N! = " + str(five_factorial_recursive) + " computation time: " + str(recursive_end - recursive_start))
 
 
     
