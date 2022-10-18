5cf60e5dd08b5ebfd22ce3e09272201f6177328e
zctaskin <zctaskin@gmail.com>
2022-10-18 13:57:45 +0300

    instance creation and initializor

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
-pass
+class Date:
+    def __init__(self) -> None:
+        self.Year = 1970
+        self.Month = 1
+        self.Day = 1
+
+today = Date()
+today.Year = 2022
+today.Month = 10
+today.Day = 18
+print(today)
+
+tomorrow = Date()
+tomorrow.Year = 2022
+tomorrow.Month = 10
+tomorrow.Day = 19
+print(tomorrow)
+
+
+
