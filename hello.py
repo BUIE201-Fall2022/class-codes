ca694cef9a889890f84e5a1c82eb15f79760229c
zctaskin <zctaskin@gmail.com>
2022-10-18 14:06:35 +0300

    added print function

--- hello.py
+++ hello.py
@@ -3,18 +3,23 @@ class Date:
         self.Year = 1970
         self.Month = 1
         self.Day = 1
+    
+    def print(self):
+        print("The date is: {}/{}/{}".format(self.Day, self.Month, self.Year))
 
 today = Date()
 today.Year = 2022
 today.Month = 10
 today.Day = 18
 print(today)
+today.print()
 
 tomorrow = Date()
 tomorrow.Year = 2022
 tomorrow.Month = 10
 tomorrow.Day = 19
 print(tomorrow)
+tomorrow.print()
 
 
 
