0ac4b46ec36ba485eb6c5db9710b179abeb9f330
zctaskin <zctaskin@gmail.com>
2022-11-17 10:40:12 +0300

    quiz3

---
+++ quiz3.py
@@ -0,0 +1,48 @@
+# O(N^2)
+def check_duplicate_n2(check):
+    n_elements = len(check)
+    if (n_elements <= 1):
+        return None
+    for i in range(n_elements - 1):
+        for j in range(i+1, n_elements):
+            if check[i] == check[j]:
+                return check[i]
+    return None
+
+def check_duplicates_nlogn(check):
+    n_elements = len(check)
+    if (n_elements <= 1):
+        return None
+    check_sorted = sorted(check)
+    # in-place sorting of the original list check.sort()
+    for i in range(n_elements - 1):
+        if check_sorted[i] == check_sorted[i+1]:
+            return check_sorted[i]
+    return None
+
+# O(n)
+def check_duplicates_n(check):
+    already_seen = set()
+    for number in check:
+        if number in already_seen:
+            return number
+        else:
+            already_seen.add(number)
+    return None
+
+check = [1, -2, 4, 7, -3, 5, 4, 5]
+result1n2 = check_duplicate_n2(check)
+print(result1n2)
+result1nlogn = check_duplicates_nlogn(check)
+print(result1nlogn)
+result1n = check_duplicates_n(check)
+print(result1n)
+
+check2 = [1, -2, 4, 7, -3, 5]
+result2n2 = check_duplicate_n2(check2)
+print(result2n2)
+result2nlogn = check_duplicates_nlogn(check2)
+print(result2nlogn)
+result2n = check_duplicates_n(check2)
+print(result2n)
+
