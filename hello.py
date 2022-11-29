a2dceb75efeda4e5b9fd8bd99c7cff556349e8dd
zctaskin <zctaskin@gmail.com>
2022-11-29 14:20:47 +0300

    linked list insert

--- hello.py
+++ hello.py
@@ -1,48 +1,39 @@
-class Array:
+class ListNode:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+class LinkedList:
     def __init__(self) -> None:
-        self._count = 0
-        self._array = None
-    
-    # O(n)
-    def insert(self, value):
-        # create a bigger array -> O(1)
-        new_count = self._count + 1
-        new_array = [None] * new_count
-        
-        # copy existing elements to the new array -> O(n)
-        for i in range(self._count):
-            new_array[i] = self._array[i]
-        
-        # add the new value at the last index -> O(1)
-        new_array[new_count - 1] = value
-
-        # update member variables -> O(1)
-        self._count = new_count
-        self._array = new_array
-    
-    # equivalent to in operator on list
-    # O(n)
-    def find(self, value):
-        for i in range(self._count):
-            if self._array[i] == value:
-                return True
-        return False
+        self.root = None
     
     # O(1)
-    def size(self):
-        return self._count
+    def insert_begin(self, value):
+        if self.root == None:
+            self.root = ListNode(value)
+        else:
+            new_node = ListNode(value)
+            new_node.next = self.root
+            self.root = new_node
     
-    # O(1)
-    def get_index(self, index):
-        if index >= 0 and index < self._count:
-            return self._array[index]
-        # Error
-        return None
-        
-my_array = Array()
-my_array.insert(15)
-my_array.insert(10)
-my_array.insert(3)
+    # O(n)
+    def insert_end(self, value):
+        if self.root == None:
+            self.root = ListNode(value)
+        else:
+            new_node = ListNode(value)
+            current_node = self.root
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
 
-result10 = my_array.find(10)
-result20 = my_array.find(20)
+my_list = LinkedList()
+my_list.insert_begin(15)
+my_list.insert_begin(10)
+my_list.insert_begin(5)
+my_list.insert_begin(3)
+my_list.insert_begin(20)
+
+my_list.insert_end(40)
+
+i = 5
