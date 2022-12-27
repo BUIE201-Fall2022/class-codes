1cfa4c214d9ae1f3d79f1969b91eba5c1b0b35d3
zctaskin <zctaskin@gmail.com>
2022-12-27 13:40:08 +0300

    binary search

--- hello.py
+++ hello.py
@@ -1,39 +1,31 @@
-print (hash(1))
-print (hash(1.5))
-print (hash("caner"))
+my_list = [3, 1, 6, 10, 4]
 
-class HashTable:
-    def __init__(self) -> None:
-        self.bucket_size = 100
-        self.hash_table = [None] * self.bucket_size
-    
-    # O(1) on average
-    def insert(self, key):
-        index = hash(key) % self.bucket_size
-        if self.hash_table[index] is None:
-            self.hash_table[index] = []
-        self.hash_table[index].append(key)
-    
-    # O(1) on average
-    def find(self, key):
-        index = hash(key) % self.bucket_size
-        if self.hash_table[index] is None:
-            return False
+# linear search on list is O(n)
+# in operator does linear search on lists
+# find(my_list, 10)
+
+my_sorted_list = [1, 3, 4, 6, 10]
+
+# O(log n)
+def binary_search(search, value):
+    if len(search) == 0:
+        return False
+    left = 0
+    right = len(search) - 1
+    while True:
+        if left == right:
+            return search[left] == value
+        elif right == left + 1:
+            return search[left] == value or search[right] == value
         else:
-            return key in self.hash_table[index]
+            mid = (left + right) // 2
+            if search[mid] == value:
+                return True
+            elif search[mid] > value:
+                right = mid - 1
+            else:
+                left = mid + 1
 
-
-my_hash = HashTable()
-# my_hash.insert(202032234245)
-# my_hash.insert(535353532342)
-# my_hash.insert(202032234245)
-
-# return1 = my_hash.find(4353535)
-# return2 = my_hash.find(535353532342)
-
-my_hash.insert("IE 201")
-my_hash.insert("IE 202")
-
-return1 = my_hash.find("IE 203")
-
-print(50)
+print(my_sorted_list)
+result6 = binary_search(my_sorted_list, 6)
+result2 = binary_search(my_sorted_list, 2)
